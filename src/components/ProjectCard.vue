8314a9c8e8e8518663bc4cfd2925d3b69cf5e1b0
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-30 18:05:15 +0200

    add pagination

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -19,11 +19,11 @@ export default {
 
 <template>
 
-  <div class="container">
+  <div class="container mb-3">
     <div class="row">
-      <div v-for="project in projects.data" class="col">
+      <div v-for="project in projects.data" class="col-4 g-3">
 
-        <div class="card">
+        <div class="card h-100">
           <div class="title_box d-flex bg-gradient justify-content-between bg-secondary p-3">
 
             <h3 class="card-title text-light fw-bolder pt-2">
@@ -53,10 +53,38 @@ export default {
     </div>
   </div>
 
+  <div class="d-flex justify-content-center paginator-pp">
+
+    <nav aria-label="Page navigation">
+      <ul
+      class="pagination"
+      >
+      <li class="page-item disabled">
+        <a class="page-link" :href="projects.prev_page_url" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item active" aria-current="page">
+        <a class="page-link" :href="projects.first_page_url">1</a>
+      </li>
+      <li class="page-item"><a class="page-link" :href="projects.next_page_url">2</a></li>
+      <li class="page-item">
+        <a class="page-link" :href="projects.last_page_url">3</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" :href="projects.next_page_url" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
+  
+  
 </template>
 
 <style lang="css">
 
-  
+
 
 </style>
