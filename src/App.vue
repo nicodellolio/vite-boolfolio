c937bcef2384961a44d4c17b090218c224f1b9c1
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-30 17:35:10 +0200

    add ProjectCard component + diplay projects

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <script>
 import axios from 'axios';
 
+import ProjectCard from "./components/ProjectCard.vue";
+
 
 export default{
   name: 'App',
+  components:{
+    ProjectCard
+  },
+
   data() {
     return{
+      'base_api_url' : 'http://127.0.0.1:8000',
+      'base_projects_url' : '/api/projects',
       'projects' : []
     }
   },
+  methods: {
+    callApi(url){
+
+      axios
+      .get(url)
+      .then(response => {
+        console.log(response);
+        this.projects = response.data.projects
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    }
+  },
   mounted(){
-    axios
-    .get('http://127.0.0.1:8000/api/project')
-    .then(response => {
-      console.log(response);
-    })
+    let url = this.base_api_url + this.base_projects_url
+    this.callApi(url)
   }
 }
 </script>
 
 <template>
+
+<ProjectCard :projects = "projects"/>
 
 </template>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,62 @@
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'ProjectCard',
+
+  props: {
+    projects: Array,
+  },
+
+  methods: {
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <div class="row">
+      <div v-for="project in projects.data" class="col">
+
+        <div class="card">
+          <div class="title_box d-flex bg-gradient justify-content-between bg-secondary p-3">
+
+            <h3 class="card-title text-light fw-bolder pt-2">
+              {{ project.title }}
+            </h3>
+
+          </div>
+
+          <div class="card-body text-secondary">
+            <h5 class="d-inline-block">Description:</h5>
+            <p class="card-text">
+              {{ project.description }}
+            </p>
+            <hr>
+
+            <div class="project-date">
+              <div class="projectStart">
+                Start Date: {{ project.project_start_date }}
+              </div>
+              <div class="projectEnd">
+                End Date: {{ project.project_end_date }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style lang="css">
+
+  
+
+</style>
