e92f50b212536b55671e6ed09727438f3a043010
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-06-04 00:50:46 +0200

    style fix to 404 page

--- src/components/views/NotFound.vue
+++ src/components/views/NotFound.vue
@@ -4,7 +4,7 @@ export default {
     name: 'NotFound',
     data() {
         return {
-            error_message : 'I couldn\'t find what you\'re looking for',
+            error_message: 'I couldn\'t find what you\'re looking for',
             chatLeft: 'Hei, you guys know this guy?',
             chatRight: 'Yeah, no, no idea'
         }
@@ -20,56 +20,34 @@ export default {
             <div class="error_message">{{ error_message }}</div>
             <router-link class="btn btn-warning fw-light fs-5" to="/">Better go Home</router-link>
         </div>
-        <div class="not-found-container w-50 container mt-5">
-            <div class="chat-left">
-                {{ chatLeft }}
-            </div>
-            <div class="chat-right">
-                {{ chatRight }}
-            </div>
-        </div>
     </div>
+    <i class="fa-solid fa-people-pulling"></i>
 </template>
 
 
 <style lang="scss" scoped>
-.not-found-container {
-    background-image: url('png-not-found.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: 100%;
-    height: 550px;
-    position: absolute;
-    bottom: 2rem;
-    right: 0;
-    .chat-left{
-        position: absolute;
-        top: 10%;
-        left: 36%;
-        width: 20%;
-        color: rgba(0, 65, 0, 0);
-        font-weight: 700;
-        font-size: 1.1rem;
-    }
-    .chat-right{
-        position: absolute;
-        top: 10%;
-        right: 19%;
-        width: 15%;
-        color: rgba(51, 2, 51, 0);
-        text-align: center;
-        padding: 0rem 0 0.2rem 0 ;
-        font-weight: 700;
-        font-size: 1.1rem;
-    }
+* {
+    overflow: hidden;
 }
-.error_number{
+
+.fa-people-pulling {
+    font-size: 45rem;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    color: #4a0336;
+    z-index: -1;
+
+}
+
+.error_number {
     font-size: 20rem;
     margin: rem 0 0 3rem;
     font-weight: 900;
     color: rgb(213, 4, 213);
 }
-.error_message{
+
+.error_message {
     color: rgb(145, 3, 145);
     font-size: 4rem;
     margin: -6rem 0 0 0rem;
